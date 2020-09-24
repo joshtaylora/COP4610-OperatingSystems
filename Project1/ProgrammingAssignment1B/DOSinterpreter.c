2b5c79d86c3aa2ac1e7765eed288520ad44b0292
joshtaylora <joshuataylora@gmail.com>
2020-09-24 12:51:58 -0400

    added final working versions of ProgrammingAssignmnet1B

--- Project1/ProgrammingAssignment1B/DOSinterpreter.c
+++ Project1/ProgrammingAssignment1B/DOSinterpreter.c
@@ -2,74 +2,34 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <iostream>
 
 
-char* UNIXconversion(char *cmd) {
-    char *UNIXcommand = malloc(6 * sizeof(char));
-
-    if (strcmp(cmd, "cd") == 0)
-    {
-        UNIXcommand = "cd";
-        return UNIXcommand;
-    }
-    else if (strcmp(cmd, "dir") == 0)
-    {
-        UNIXcommand = "ls";
-        return UNIXcommand;
-    }
-    else if (strcmp(cmd, "type") == 0)
-    {
-        UNIXcommand = "cat";
-        return UNIXcommand;
-    }
-    else if (strcmp(cmd, "del") == 0)
-    {
-        UNIXcommand = "rm";
-        return UNIXcommand;
-    }
-    else if (strcmp(cmd, "ren") == 0)
-    {
-        UNIXcommand = "mv";
-        return UNIXcommand;
-    }
-    else if (strcmp(cmd, "copy") == 0)
-    {
-        UNIXcommand = "cp";
-        return UNIXcommand;
-    }
-    else
-    {
-        return NULL;
-    }
-    
-}
-
-int commandEnter(char *cmd, char *arg1, char *arg2)
+int commandEnter(char *UNIXcommand, char *arg1, char *arg2)
 {
-    if (strcmp(cmd, "cd") == 0 && sizeof(arg1) > 0 && sizeof(arg2) == 0)
+    if (strcmp(UNIXcommand, "cd") == 0 && sizeof(arg1) > 0)
     {
-        chdir
+        //printf("chdir(%s)\n", arg1);
+        chdir(arg1);
     }
-    else if (strcmp(cmd, "dir") == 0)
+    else if (strlen(UNIXcommand) != 0 && sizeof(arg1) != 0 && sizeof(arg2) != 0)
     {
-        
+        strcat(UNIXcommand, " ");
+        strcat(UNIXcommand, arg1);
+        strcat(UNIXcommand, " ");
+        strcat(UNIXcommand, arg2);
+        system(UNIXcommand);
+        return 0;
     }
-    else if (strcmp(cmd, "type") == 0)
+    else if (strcmp(UNIXcommand, "ls") == 0)
     {
-        
+        system(UNIXcommand);
+        return 0;
     }
-    else if (strcmp(cmd, "del") == 0)
+    else if (strcmp(UNIXcommand, "cat") == 0) 
     {
-        
-    }
-    else if (strcmp(cmd, "ren") == 0)
-    {
-        
-    }
-    else if (strcmp(cmd, "copy") == 0)
-    {
-        
+        strcat(UNIXcommand, " ");
+        strcat(UNIXcommand, arg1);
+        system(UNIXcommand);
     }
     return 1;
 }
@@ -78,47 +38,98 @@ int main()
 {
     int index, cmdLength, arg1Length, arg2Length;
     char line[32]; // size is prob bigger than needed tbh
-    printf("Enter DOS commands:\t");
     // reads characters from user input (stdin) and stores them in the character array called line
-    while (fgets(line, 32, stdin) == NULL)
+    //printf("Type Ctrl-c to end command interpretation\n");
+    printf("Enter DOS commands\n");
+    
+    index = 0;
+
+    while (fgets(line, 100, stdin))
     {
-        char *command = malloc(6 * sizeof(char)); // allocate memory for a string of 6 characters (i chose 6 just because its large enough to fit all of the commands he gave us
-        char *arg1 = malloc(20 * sizeof(char)); // allocate memory for the first argument (no idea how long this will be, could experiment with different sizes later
-        char *arg2 = malloc(20 * sizeof(char)); // allocate memory for the second argument (no idea how long this will be, could experiment with different sizes later
+        index = 0;
+        
+        //printf("Enter DOS commands:\n");
+        // allocate memory for the character sequences
+        char *command = malloc(50 * sizeof(char));
+        char *arg1 = malloc(20 * sizeof(char));
+        char *arg2 = malloc(20 * sizeof(char));
+
+        int wspLength;
 
 
-        const char* wsp = " \t"; // will be used to split the input from the first command to the first occurrence of a space or tab character
+        const char* wsp = " \t\n\r"; // will be used to split the input from the first command to the first occurrence of a space or tab character
 
         cmdLength = strcspn(line, wsp); // counts the number of contiguous characters that are NOT whitespace characters (stops when it reaches the first occurrence of a space or tab char
 
         strncpy(command, line, cmdLength); // copy into the string 'command' characters from 'line' starting at index 0 and continuing until 'cmdLength' characters have been read
+        //printf("cmd:\t%s\n",command);
 
-        index += cmdLength + 1;
+        index += cmdLength;
 
+        wspLength = strspn(line + index, wsp);
+        index += wspLength;
         arg1Length = strcspn(line + index, wsp);
         strncpy(arg1, line + index, arg1Length); 
+        //printf("arg1:\t%s\n", arg1);
         
-        index += arg1Length + 1;
+        index += arg1Length;
 
+        wspLength = strspn(line + index, wsp);
+        index += wspLength;
         arg2Length = strcspn(line + index, wsp);
         strncpy(arg2, line + index, arg1Length); 
+        //printf("arg2:\t%s\n", arg2);
 
-        printf("DOS: command: %s\tlength = %d\n\targ1: %s\tlength = %d\n\targ2: %s\tlength = %d\n", 
-        command, (int)strlen(command), arg1, (int)strlen(arg1), arg2, (int)strlen(arg2));
+        ////printf("\n\tDOS: command: %s\tlength = %d\n\targ1: %s\tlength = %d\n\targ2: %s\tlength = %d\n", command, (int)strlen(command), arg1, (int)strlen(arg1), arg2, (int)strlen(arg2));
 
-        char *UNIXcommand = malloc(6 * sizeof(char));
-        char *UNIXarg1 = malloc(20 * sizeof(char));
-        char *UNIXarg2 = malloc(20 * sizeof(char));
+        char *UNIXcommand = malloc(50 * sizeof(char));
 
-        UNIXcommand = UNIXconversion(command);
-        if (UNIXcommand != NULL)
+        if (strcmp(command, "cd") == 0)
         {
-            
+            strcpy(UNIXcommand, "cd");
+            //printf("UNIX:\t%s\n", UNIXcommand);
+        }
+        else if (strcmp(command, "dir") == 0)
+        {
+            strcpy(UNIXcommand, "ls");
+            //printf("UNIX:\t%s\n", UNIXcommand);
+        }
+        else if (strcmp(command, "type") == 0)
+        {
+            strcpy(UNIXcommand, "cat");
+            //printf("UNIX:\t%s\n", UNIXcommand);
+        }
+        else if (strcmp(command, "del") == 0)
+        {
+            strcpy(UNIXcommand, "rm");
+            //printf("UNIX:\t%s\n", UNIXcommand);
+        }
+        else if (strcmp(command, "ren") == 0)
+        {
+            strcpy(UNIXcommand, "mv");
+            //printf("UNIX:\t%s\n", UNIXcommand);
+        }
+        else if (strcmp(command, "copy") == 0)
+        {
+            strcpy(UNIXcommand, "cp");
+            //printf("UNIX:\t%s\n", UNIXcommand);
         }
         else
         {
-            printf("ERROR: DOS command not recognized\n");
+            //printf("ERROR: DOS command not recognized\n");
         }
+
+
+        if (UNIXcommand != NULL)
+        {
+            commandEnter(UNIXcommand, arg1, arg2);
+            //printf("\tUNIX command is now:\t%s\n", UNIXcommand);
+        }
+        else
+        {
+            //printf("ERROR: DOS command not recognized\n");
+        }
+        printf("Type Ctrl-c to end command interpretation\n");
 
         
     }
